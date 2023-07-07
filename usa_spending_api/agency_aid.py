8a2c229e91463393c8cfec7a4095bad34193b586
Nguyen Doan <36469082+nguyendoanbb@users.noreply.github.com>
2023-07-07 15:48:45 -0400

    Removed some parts

--- usa_spending_api/agency_aid.py
+++ usa_spending_api/agency_aid.py
@@ -9,20 +9,29 @@ load_dotenv("./env_info/.env")
 url = os.getenv('usaspending_url')
 endpoint = "/api/v2/autocomplete/accounts/aid/"
 
-values = []
-for i in range(0, 10):
-    body = {
-    "filters": {
-        "aid": str(i),
-    },
-    "limit": 500,
-    }
-    response = requests.post(url + endpoint, json=body)
+# create a class to pull data from the USA Spending API
+class agency_aid:
+    def __init__(self, url, endpoint):
+        self.url = url
+        self.endpoint = endpoint
 
-    for key in response.json():
-        for element in response.json()[key]:
-            values.append(element)
+    def get_data():
+        values = []
+        for i in range(0, 10):
+            body = {
+                "filters": {
+                    "aid": str(i),
+                    },
+                    "limit": 500,
+                    }
+            response = requests.post(url + endpoint, json=body)
+            for key in response.json():
+                for element in response.json()[key]:
+                    values.append(element)
 
-# turn list of dictionaries into a pandas table
-df = pd.DataFrame(values)
-print(df.head())
+        # turn list of dictionaries into a pandas table
+        df = pd.DataFrame(values)
+        print(df.head(10))
+
+agency_aid.get_data()
+
