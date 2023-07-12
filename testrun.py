64250e217e2414040e77d154e6f0e7eecbc94812
Nguyen Doan <36469082+nguyendoanbb@users.noreply.github.com>
2023-07-11 23:39:47 -0400

    no changes

--- testrun.py
+++ testrun.py
@@ -2,6 +2,5 @@ import usa_spending_api
 
 AID = usa_spending_api.api_pull.agency_aid()
 # usa_spending_api.api_pull.agency_overview(aid_list=AID) 
-federal_acc, treasury_acc = usa_spending_api.api_pull.agency_federal_account(aid_list=AID)
-print(federal_acc)
-print(treasury_acc)
+test = usa_spending_api.api_pull.agency_budgetary_resources(aid_list=AID)
+print(test)
